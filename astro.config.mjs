1af6eaf6666b0ac348c8eacf7310b0aae9493354
Brian <contactbrianlopez@gmail.com>
2023-10-04 10:56:57 -0700

    Added Sitemap Generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  site: 'https://chavezframing.com',
+  integrations: [sitemap({
+    filter: (page) => page !== 'https://chavezframing.com/privacy-policy'
+  }), react()],
+  outDir: './site',
+  compressHTML: true,
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+})
